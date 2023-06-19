3a14d84b4391818c3e3e00a128da4d2751bfcc53
storm313 <36401899+storm313@users.noreply.github.com>
2023-06-19 17:21:36 +0530

    Add files via upload

---
+++ PROC_EMP_CURSOR.sql
@@ -0,0 +1,19 @@
+create or replace
+--To add an exception block type excep and the press Ctrl+Shift+t.
+PROCEDURE proc_emp_cursor is
+  CURSOR c_emp_cursor IS 
+   SELECT empno, ename FROM emp
+   WHERE deptno =30;
+  v_empno emp.empno%TYPE;
+  v_lname emp.ename%TYPE;
+BEGIN  OPEN c_emp_cursor;
+  LOOP
+    FETCH c_emp_cursor INTO v_empno, v_lname;
+    EXIT WHEN c_emp_cursor%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE( v_empno ||'  '||v_lname);  
+  END LOOP;
+EXCEPTION
+WHEN NO_DATA_FOUND THEN
+DBMS_OUTPUT.PUT_LINE('The query did not return a result');
+END;
+/
